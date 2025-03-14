86a08be1f2bd8732d039bbf02d2879f1abc183aa
Nayara <nayarag.silva@hotmail.com>
2025-03-13 22:02:27 -0300

    Delete atividade directory

--- atividade/routes/testRoute.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const testcontroller = require ('../controllers/testController');
-
-rouber.get('/test', testcontroller.teste);
-
-module.exports = router; 
