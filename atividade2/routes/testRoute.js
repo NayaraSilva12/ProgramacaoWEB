a6e356cc664e58cd2d6fea41ab12623056937445
Nayara <nayarag.silva@hotmail.com>
2025-03-20 22:13:40 -0300

    Add files via upload

---
+++ atividade2/routes/testRoute.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+const testcontroller = require ('../controllers/testController');
+
+router.get('/test', testcontroller.testeGet);
+router.get('/check', testcontroller.validarGet);
+
+router.post('/check', testcontroller.validarPost);
+router.post('/check', testcontroller.validarPost);
+
+module.exports = router; 
