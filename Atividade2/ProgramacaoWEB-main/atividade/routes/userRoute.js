d94f97785869d7014182e42428ac85ccdbefab2e
Nayara <nayarag.silva@hotmail.com>
2025-03-20 22:08:28 -0300

    Delete Atividade2/ProgramacaoWEB-main directory

--- Atividade2/ProgramacaoWEB-main/atividade/routes/userRoute.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const userController = require ('../controllers/userController');
-
-router.get('/listusers', userController.listUsersGet);
-router.ger('/listuser/:id', userController.listUserGet);
-modeule.exports = router;
